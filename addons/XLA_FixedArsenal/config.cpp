c31de9d0ed157f3fd5f91a98df8803079d87c9af
Alexander (ImperialAlex) <ImperialAlex@gmail.com>
2014-12-13 12:30:50 +0100

    Move functions to subfolder

--- addons/XLA_FixedArsenal/config.cpp
+++ addons/XLA_FixedArsenal/config.cpp
@@ -18,20 +18,20 @@ class CfgFunctions {
 		tag = XLA;
 
 		class Inventory {
-			class loadPartialInventory {file = "\XLA_FixedArsenal\Inventory\fn_loadPartialInventory.sqf";};
-			class arsenal {file = "\XLA_FixedArsenal\Inventory\fn_arsenal.sqf";};
-			class addVirtualBackpackCargo {file = "\XLA_FixedArsenal\Inventory\fn_addVirtualBackpackCargo.sqf";};
-			class addVirtualItemCargo {file = "\XLA_FixedArsenal\Inventory\fn_addVirtualItemCargo.sqf";};
-			class addVirtualMagazineCargo {file = "\XLA_FixedArsenal\Inventory\fn_addVirtualMagazineCargo.sqf";};
-			class addVirtualWeaponCargo {file = "\XLA_FixedArsenal\Inventory\fn_addVirtualWeaponCargo.sqf";};
-			class getVirtualBackpackCargo {file = "\XLA_FixedArsenal\Inventory\fn_getVirtualBackpackCargo.sqf";};
-			class getVirtualItemCargo {file = "\XLA_FixedArsenal\Inventory\fn_getVirtualItemCargo.sqf";};
-			class getVirtualMagazineCargo {file = "\XLA_FixedArsenal\Inventory\fn_getVirtualMagazineCargo.sqf";};			
-			class getVirtualWeaponCargo {file = "\XLA_FixedArsenal\Inventory\fn_getVirtualWeaponCargo.sqf";};
-			class removeVirtualBackpackCargo {file = "\XLA_FixedArsenal\Inventory\fn_removeVirtualBackpackCargo.sqf";};
-			class removeVirtualItemCargo {file = "\XLA_FixedArsenal\Inventory\fn_removeVirtualItemCargo.sqf";};
-			class removeVirtualMagazineCargo {file = "\XLA_FixedArsenal\Inventory\fn_removeVirtualMagazineCargo.sqf";};
-			class removeVirtualWeaponCargo {file = "\XLA_FixedArsenal\Inventory\fn_removeVirtualWeaponCargo.sqf";};
+			class loadPartialInventory {file = "\XLA_Fixedarsenal\functions\Inventory\fn_loadPartialInventory.sqf";};
+			class arsenal {file = "\XLA_Fixedarsenal\functions\Inventory\fn_arsenal.sqf";};
+			class addVirtualBackpackCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_addVirtualBackpackCargo.sqf";};
+			class addVirtualItemCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_addVirtualItemCargo.sqf";};
+			class addVirtualMagazineCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_addVirtualMagazineCargo.sqf";};
+			class addVirtualWeaponCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_addVirtualWeaponCargo.sqf";};
+			class getVirtualBackpackCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_getVirtualBackpackCargo.sqf";};
+			class getVirtualItemCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_getVirtualItemCargo.sqf";};
+			class getVirtualMagazineCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_getVirtualMagazineCargo.sqf";};			
+			class getVirtualWeaponCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_getVirtualWeaponCargo.sqf";};
+			class removeVirtualBackpackCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_removeVirtualBackpackCargo.sqf";};
+			class removeVirtualItemCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_removeVirtualItemCargo.sqf";};
+			class removeVirtualMagazineCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_removeVirtualMagazineCargo.sqf";};
+			class removeVirtualWeaponCargo {file = "\XLA_Fixedarsenal\functions\Inventory\fn_removeVirtualWeaponCargo.sqf";};
 		};
 	};
 };
