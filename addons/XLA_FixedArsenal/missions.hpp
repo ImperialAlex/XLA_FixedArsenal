6143213cde577fa36c7b5e227bd0f14e39279de1
Alexander (ImperialAlex) <ImperialAlex@gmail.com>
2015-01-10 17:52:01 +0100

    add missions.hpp and activate it

---
+++ addons/XLA_FixedArsenal/missions.hpp
@@ -0,0 +1,16 @@
+
+class CfgMissions
+{
+	class Showcases
+	{
+    class XLA_Showcase
+		{
+			briefingName = "XLA | FixedArsenal Showcase";
+			directory = "XLA_FixedArsenal\sample_missions\Showcases\FixedArsenalExample.Stratis";
+			overviewText = "This mission features a simple example of the FixedArsenal";
+			author = "Alexander (ImperialAlex)";
+		};
+	
+	};
+
+};
