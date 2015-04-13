a3fd764743ae6f3b2b0df6e1aa6865de96f06a2b
Alexander (ImperialAlex) <ImperialAlex@gmail.com>
2015-04-13 19:46:02 +0200

    add mod.cpp and logos

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+name = "XLA Fixed Arsenal v2.1.0";
+picture = "XLA_FixedArsenal_Picture.paa";
+actionName = "Website";
+action = "https://github.com/ImperialAlex/XLA_FixedArsenal";
+description = "XLA Fixed Arsenal";
+logo = "XLA_logo_highlight.paa";
+logoOver = "XLA_logo_black.paa";
+tooltip = "XLA Fixed Arsenal";
+tooltipOwned = "XLA Fixed Arsenal";
+overview = "This addon adds a 'fixed' version of arsenal (XLA_fnc_arsenal) that enables partial outfit loading and generally improves whitelisting support.";
+author = "Alexander (ImperialAlex)";
+overviewPicture = "XLA_logo_black.paa";
+overviewText = "This addon adds a 'fixed' version of arsenal (XLA_fnc_arsenal) that enables partial outfit loading and generally improves whitelisting support.";
+overviewFootnote = "XLA Fixed Arsenal by Alexander (ImperialAlex)";
